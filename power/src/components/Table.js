9248416c7c7ac9fb90daccb675755c3f21354ddb
Jacob Kjærsgaard Hansen <jacobkjaersgaardhansen@Jacobs-MacBook-Pro-8.local>
2020-02-18 18:47:44 +0100

    simple table works

---
+++ power/src/components/Table.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+
+const msToLoad = 10000;
+
+export default function Table(){
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getLast = async () => {
+      const response = await fetch(`https://power.labitat.dk/last/${msToLoad}`);
+      response.status === 200 && setData(await response.json());
+    };
+    getLast();
+  }, []);
+
+  useEffect(() => {
+    let cancelled = false;
+    const getBlip = async () => {
+      const response = await fetch('https://power.labitat.dk/blip');
+      if(response.status === 200){
+        const blip = await response.json();
+        setData(d => [...d, blip]);
+      }
+      cancelled || getBlip();
+    }
+    getBlip();
+    return () => cancelled = true;
+  }, []);
+
+  const rows = data.sort((a, b) => b[0] - a[0]).map(d => {
+    const id = d[0];
+    const timestamp = new Date(d[0]);
+    const msSinceLast = d[1];
+    const power = 3600000 / msSinceLast;
+    return (
+      <tr key={id}>
+        <td>{timestamp.toLocaleTimeString()}</td>
+        <td>{msSinceLast} ms</td>
+        <td>{power.toFixed()} W</td>
+      </tr>
+    )
+  })
+
+  return (
+    <div>
+      <table style={{ width: "100%" }}>
+        <thead>
+          <tr>
+            <th>Timestamp</th>
+            <th>Time between blips</th>
+            <th>Power</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  );
+}
