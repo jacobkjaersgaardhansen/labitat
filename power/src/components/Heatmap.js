cca0be215879bbb44dabef9d37feae2a7e327fb7
Jacob Kjærsgaard Hansen <jacobkjaersgaardhansen@jacobs-mbp-8.home>
2020-02-19 10:58:03 +0100

    clean up

--- power/src/components/Heatmap.js
+++ power/src/components/Heatmap.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Heatmap(){
-  const [timeStart, setTimeStart] = useState('2020-01-01');
-  const [timeEnd, setTimeEnd] = useState('2020-02-17');
+  const [dateStart, setdateStart] = useState('2020-01-01');
+  const [dateEnd, setdateEnd] = useState('2020-02-17');
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    const timeStart = new Date(`${dateStart} 00:00:00.000`).getTime();
+    const timeEnd = new Date(`${dateEnd} 23:59:59.999`).getTime();
     const getHourly = async () => {
-      const response = await fetch(`https://power.labitat.dk/hourly/${new Date(timeStart).getTime()}/${new Date(timeEnd).getTime()}`);
+      const response = await fetch(`https://power.labitat.dk/hourly/${timeStart}/${timeEnd}`);
       response.status === 200 && setData(await response.json());
     };
     getHourly();
-  }, [timeStart, timeEnd]);
+  }, [dateStart, dateEnd]);
   
   const dataEnriched = data.map(d => {
     const timestamp = new Date(d[0]);
     return { 
       timeslot: timestamp.getHours(),
-      day: timestamp.getDay() === 0 ? 7 : timestamp.getDay(), // moving Sunday to end of week instead of start of week
+      day: (timestamp.getDay() - 1) % 7, // moving Sunday to end of week instead of start of week
       powerConsumption: d[2],
       longestBlipGap: d[4],
       shortestBlipGap: d[3],
@@ -26,32 +28,13 @@ export default function Heatmap(){
     }
   });
 
-  const dataGrouped = dataEnriched.reduce((acc, obj) => {
-    let key1 = obj.timeslot
-    let key2 = obj.day;
-    if(!acc[key1]){
-      acc[key1] = {};
-    }
-    if(!acc[key1][key2]){
-      acc[key1][key2] = [];
-    }
-    acc[key1][key2].push(obj);
-    return acc
-  }, {});
-
   const rows = [];
   for (let timeslot = 0; timeslot < 24; timeslot++){
     const days = [];
-    for (let day = 1; day < 8; day++){
-      let powerConsumption = 0;
-      let dataExists = false;
-      if(dataGrouped[timeslot]){
-        if(dataGrouped[timeslot][day]){
-          powerConsumption = dataGrouped[timeslot][day].reduce((avg, val, _, { length }) => avg + val.powerConsumption / length, 0);
-          dataExists = true;
-        }
-      }
-      days[day] = (dataExists ? 
+    for (let day = 0; day < 7; day++){
+      const dataPoints = dataEnriched.filter(d => d.timeslot === timeslot && d.day === day);
+      const powerConsumption = dataPoints.reduce((avg, value, _, { length }) => avg + value.powerConsumption / length, 0);
+      days.push(dataPoints.length > 0 ? 
         <td style={{
           color: powerConsumption < 1000 ? 'lightgrey' : 'black', 
           backgroundColor: `rgb(${powerConsumption / 1500 * 256}, 0, 0)`,
@@ -60,22 +43,21 @@ export default function Heatmap(){
         <td></td>
       )
     }
-    rows[timeslot] = (
+    rows.push(
       <tr>
-        <td style={{ textAlign: 'center' }}>{timeslot}:00-{timeslot+1}:00</td>
+        <td style={{ textAlign: 'center' }}>{timeslot}:00 - {timeslot+1}:00</td>
         {days}
       </tr>
     )
   }
   
-
   return (
     <div>
       <form>
         <label htmlFor="start">Start date</label>
-        <input id='start' type="date" value={timeStart} onChange={e => setTimeStart(e.target.value)} />
+        <input id='start' type="date" value={dateStart} onChange={e => setdateStart(e.target.value)} />
         <label htmlFor="end">End date</label>
-        <input id='end' type="date" value={timeEnd} onChange={e => setTimeEnd(e.target.value)} />
+        <input id='end' type="date" value={dateEnd} onChange={e => setdateEnd(e.target.value)} />
       </form>
       <table style={{ width: "100%" }}>
         <thead>
