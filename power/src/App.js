8f79ed0753433942b3025e0e98f9513903cca3d9
Jacob Kjærsgaard Hansen <jacobkjaersgaardhansen@jacobs-mbp-8.home>
2020-02-19 14:33:32 +0100

    Footer added

--- power/src/App.js
+++ power/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './bootstrap.css';
 import Table from './components/Table';
 import Heatmap from './components/Heatmap';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Heatmap />
       <Table />
+      <Footer />
     </div>
   );
 }
